dc40d3d5debbf822022802717b308d88dbd5eb19
冯广蕾 <fgl890317@163.com>
2015-09-12 20:18:02 +0800

    新增数据类

---
+++ app/src/main/java/app/coolweather/com/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package app.coolweather.com.coolweather.model;
+
+/**
+ * Created by Administrator on 2015/9/10.
+ */
+public class Province {
+    private int id;
+    private String provinceCode;
+    private String provinceName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
